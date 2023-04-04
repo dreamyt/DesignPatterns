0e12c4b674357ce53880f3cd7f7f6c88d8292e87
lilacwine <58385035+dreamyt@users.noreply.github.com>
2023-04-04 16:38:49 +0800

    Bridge

---
+++ Bridge/bridge/example/remotes/Remote.java
@@ -0,0 +1,16 @@
+package bridge.example.remotes;
+
+/**
+ *
+ */
+public interface Remote {
+    void power();
+
+    void volumeDown();
+
+    void volumeUp();
+
+    void channelDown();
+
+    void channelUp();
+}
