b89f10a1149531d250608e35228385a143cf135b
lilacwine <58385035+dreamyt@users.noreply.github.com>
2023-04-04 15:49:25 +0800

    Adapter

---
+++ Adapter/adapter/example/round/RoundHole.java
@@ -0,0 +1,19 @@
+package adapter.example.round;
+
+/**
+ *
+ */
+public class RoundHole {
+    private double radius;
+
+    public RoundHole(double radius){
+        this.radius = radius;
+    }
+    public double getRadius(){
+        return radius;
+    }
+
+    public boolean fits(RoundPeg peg){
+        return this.radius >= peg.getRadius();
+    }
+}
